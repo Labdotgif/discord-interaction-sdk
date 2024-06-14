9c7474a0f7fdc9ef8ded5797c888b1e62a905bb7
Sylvain <sylvain@instant-gaming.com>
2024-06-14 17:31:56 +0200

    fix: user, add hasPermission.

--- src/Service/DiscordUserService.php
+++ src/Service/DiscordUserService.php
@@ -9,6 +9,9 @@ class DiscordUserService extends AbstractDiscordService
     private const API_ROUTE_ME = '/users/@me';
     private const API_ROUTE_ME_GUILDS = self::API_ROUTE_ME . '/guilds';
 
+    public const int PERMISSION_ADMINISTRATOR = 1 << 3;
+    public const int PERMISSION_MANAGE_GUILD = 1 << 5;
+
     public function __construct(DiscordHttpClient $httpClient, string $userAccessToken = null)
     {
         parent::__construct($httpClient, $userAccessToken);
@@ -24,5 +27,10 @@ class DiscordUserService extends AbstractDiscordService
     public function guilds(): array
     {
         return $this->get(self::API_ROUTE_ME_GUILDS);
+    }
+
+    public function hasPermission(string $userPermissions, int $permission): bool
+    {
+        return $userPermissions & $permission;
     }
 }
